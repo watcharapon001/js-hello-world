50d0eb72f2fc7b5483df7abb02da84dfa60624f6
watcharapon001 <watcharapon590@gmail.com>
2022-08-28 11:39:41 +0700

    ggg

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีจ้าจักวาลกาแล็กซี่เองจ้า YT:Jakkawal Galaxy</h1>')
+  return res.send('<h1>นอน</h1>')
 })
 
 const PORT = process.env.PORT || 3000
